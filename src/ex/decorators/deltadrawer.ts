46075cd3ec5f983490e3170e69b3e90f3e3ed917
Styn van de Haterd <chaosnova2011@hotmail.com>
2023-03-06 11:12:43 +0100

    Delta-border bug

--- src/ex/decorators/deltadrawer.ts
+++ src/ex/decorators/deltadrawer.ts
@@ -3,6 +3,7 @@ import {IPokemonEx} from "../pokemonex";
 
 import DeltaBorderBasic from "../../assets/ex/Borders/delta_border_basic.png";
 import DeltaBorderEvolution from "../../assets/ex/Borders/delta_border_evolution.png";
+
 import {Variant} from "../variant";
 import {IsEvolved} from "../../pokemon/evolution";
 
@@ -11,7 +12,7 @@ export class DeltaDrawer extends AbstractElementDrawer{
     public async drawElement(): Promise<void> {
         const pokemonEx = this.pokemon as IPokemonEx;
         if(pokemonEx.isDeltaSpecies && pokemonEx.variant != Variant.GOLDSTAR){
-            const deltaSpeciesBorder = $(`<img src='${pokemonEx.evolution == null ? DeltaBorderBasic : DeltaBorderEvolution}' class='poke-delta-species-border card-element'>`)
+            const deltaSpeciesBorder = $(`<img src='${IsEvolved(pokemonEx) ? DeltaBorderEvolution : DeltaBorderBasic}' class='poke-delta-species-border card-element'>`)
             deltaSpeciesBorder
                 .setElementPosition(61, 33, 0, 0)
                 .applyStyleIf("left", 42, (): boolean => {
